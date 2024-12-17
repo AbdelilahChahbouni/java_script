191a23a9f017d569023a366a5efc2d1801072077
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-12-17 12:51:50 +0100

    console methods and web API

---
+++ script4.js
@@ -0,0 +1,12 @@
+/*
+    The Output Functions:
+    -window.alert()
+    -document.write()
+    -console.log()
+
+*/
+
+window.alert("hello from js");
+document.write("<h1>helo from doc</h1>");
+console.log("hello from log");
+
---
+++ script5.js
@@ -0,0 +1,23 @@
+// console methods 
+/* 
+    -log()
+    -error()
+    -table()
+    -time()
+    -clear()
+    -group()
+    -info()
+    -warn()
+*/
+
+// Web Api
+// Console is A web api provider from the browser to debug and iteract whit the code of java script
+
+// Styling the consol output using C% DIRECTIVE
+
+
+console.log("hello from %cjs file", "color:red ; fontsize: 40px");
+console.error("this is erorr from js file");
+console.table(['data1' , 'data2' , 'data3']);
+
+
