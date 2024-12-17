7c0cb722d742eba10556ce0d5236561cddb26db3
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-12-17 12:20:22 +0100

    Comments And Bad Paractices

--- script2.js
+++ script2.js
@@ -1,1 +1,10 @@
-// where To Put The Code in Program
+// where To Put The Code js in  Program
+// you can put the code js in head or in the last in body
+// the best place is in the last of code in body
+window.onload = function(){
+    document.querySelector("h1").style.color = "blue";
+};
+
+
+
+
---
+++ script3.js
@@ -0,0 +1,11 @@
+// Comments And Bad Practices
+
+// Single Line Comment
+
+/* Multiple Line Comments */
+
+
+// the event wait the page to load
+window.onload = function(){
+    console.log("hello world");
+};
