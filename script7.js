089941aa1c365606a5b88cdfac1fae643dee0509
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-12-20 17:09:11 +0100

    types of variables part one

---
+++ script7.js
@@ -0,0 +1,21 @@
+/* 
+    Data types
+    -String
+    -Number
+    -array => object
+    -Object => dictionnery
+    -undefine
+    -null
+    -boolean
+
+*/
+
+
+console.log(typeof "hello");
+console.log(typeof 100);
+console.log(typeof [10 , 20 , 30]);
+console.log(typeof {name:"hello" , age: 20 , country: "maroc"});
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null)
+
