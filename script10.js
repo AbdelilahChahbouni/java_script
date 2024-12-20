3d62c1928a8e33b7fbc25c8603bd7534e3a6d0d1
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-12-20 18:18:39 +0100

    let and var and const , String Synthax , Escape Character Sequence

---
+++ script10.js
@@ -0,0 +1,24 @@
+/*
+    Var:
+        -Redeclare(Yes)
+        -Access Before Declare(Undifined)
+        -Variable Scope Drama(Added To Window Object)
+        -Block or Function Scope()
+    let:
+        -Redeclare(No error)
+        -Access Before Declare(Error)
+        -Variable Scope Drama()
+        -Block or Function Scope()
+    const:
+        -Redeclare(No error)
+        -Access Before Declare(Error)
+        -Variable Scope Drama()
+        -Block or Function Scope()
+
+*/
+
+
+var var1="new value";
+console.log(var1);
+
+
---
+++ script11.js
@@ -0,0 +1,14 @@
+/*
+    -String Synthax + Character Escape Sequence
+    -\ Escape + Line Continue
+    -\n new Line
+*/
+
+
+console.log("hello");
+console.log('hello');
+console.log("hello 'ahmed' ");
+console.log('hello "reda" ');
+console.log("hello \"test\" ");
+console.log("elzero\nweb\nschool");
+
