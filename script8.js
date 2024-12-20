50cab4dc7faf7a8f00c309d93187c33881090be8
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-12-20 17:32:41 +0100

    Variables and Indentifiers Conventions and Rules

---
+++ script8.js
@@ -0,0 +1,17 @@
+/*
+    variables:
+        -What is Variable
+        -Whay we use variable
+        -Declare variable and use
+        -Synthax
+        -Variable Without Var
+        -Multiple Variable in the same Line
+        -Id and Global Variable
+        -Loosley Typed and Strong Typed
+        -java script is loosley type
+*/
+
+var Name = "ahmed";
+
+console.log(Name);
+hello.innerHTML = "new value"
---
+++ script9.js
@@ -0,0 +1,12 @@
+/*
+    Indentifiers:
+    - Name Conventions And Rules
+    - Reserved Words
+*/
+
+var User = "reda";
+var user = 'ahmed';
+var userName = "tarik"
+console.log(User);
+console.log(user);
+
